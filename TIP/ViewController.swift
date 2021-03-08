1e13831d0a15bf50c33703756198532358427813
moesaifan99 <68472462+moesaifan99@users.noreply.github.com>
2021-03-07 22:34:58 -0500

    Switch Project
    swift project

--- TIP/ViewController.swift
+++ TIP/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var bill: UITextField!
+    @IBOutlet weak var percentA: UITextField!
+    @IBOutlet weak var percentB: UITextField!
+    @IBOutlet weak var percentC: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculate(_ sender: Any) {
+        if let finalBill = Double(bill.text!){
+                    
+                    
+                    
+                    let ten = finalBill * 0.1
+                    let fifteen = finalBill * 0.15
+                    let twenty = finalBill * 0.2
+                    
+                    percentA.text = String(format: "$%.2f", ten)
+                    percentB.text = String(format: "$%.2f", fifteen)
+                    percentC.text = String(format: "$%.2f", twenty)
+                    
+                }
+        
+    }
 }
 
